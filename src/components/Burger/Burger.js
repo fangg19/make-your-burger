aaa832c1efb70ac1843dcfabe316f954ff8c6347
fangg19 <alexjohnny6@gmail.com>
2020-09-05 21:04:32 +0300

    converted the state object to an array that i can map to render the ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+  // //have to convert the ingredients from the state object into an array so i can map it to render the chosen ingredients
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
-export class BurgerBuilder extends Component {
+class BurgerBuilder extends Component {
+  // old method of state init
+  // constructor (props){
+  //   super(props)
+  //   this.state = {}
+
+  // }
+
+  state = {
+    ingredients: {
+      meat: 1,
+      bacon: 1,
+      salad: 1,
+      cheese: 2,
+    },
+  };
   render() {
     return (
       <Auxiliary>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
       </Auxiliary>
     );
   }
