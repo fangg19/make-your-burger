be7dcce1ff4c62f863aa6b959a487708d518f1db
fangg19 <alexjohnny6@gmail.com>
2020-09-09 21:37:22 +0300

    Just added the backdrop for the CONTINUE modal and also added the two buttons on that modal.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
+import Button from '../../UI/Button/Button';
 
 const OrderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -16,6 +17,12 @@ const OrderSummary = (props) => {
       <p>Your delicious burger topped with the following:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout ?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Auxiliary>
   );
 };
