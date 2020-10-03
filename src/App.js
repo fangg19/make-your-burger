478186651da8945a89e68ec46523e67901b55706
fangg19 <alexjohnny6@gmail.com>
2020-10-03 21:46:32 +0300

    added the Checkout component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import classes from './App.modules.css';
 import Layout from './hoc/Layouts/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div className={classes.App}>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );
